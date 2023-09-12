a87ed5d803b6eb3747154da8cb265c62e03435e4
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-09-11 19:58:06 -0600

    Update "Cabecera_Pagina.php"

--- EJERCICIOS_POO_SOLID/Cabecera_Pagina.php
+++ EJERCICIOS_POO_SOLID/Cabecera_Pagina.php
@@ -1,45 +1,38 @@
 <?php
 //Principio de Responsabilidad Única (SRP): La clase CabeceraPagina tiene una única responsabilidad de manejar las propiedades y la apariencia del título de la página.
 
-// Clase que se encarga de manejar las propiedades y la apariencia del título de la página
 class CabeceraPagina {
     private $titulo;
     private $color;
     private $fuente;
+    private $apariencia;
 
-    // Método que se encarga de manejar las propiedades del título
-    public function obtenerPropiedades($titulo, $color, $fuente) {
+    public function __construct($titulo, $color, $fuente) {
         $this->titulo = $titulo;
         $this->color = $color;
         $this->fuente = $fuente;
     }
 
-    // Método que se encarga de manejar la apariencia del título
-    public function obtenerAparienciaTitulo($posicion) {
-        if ($posicion == "centrado") {
-            return "Título centrado";
-        } elseif ($posicion == "derecha") {
-            return "Título a la derecha";
-        } elseif ($posicion == "izquierda") {
-            return "Título a la izquierda";
-        } else {
-            return "Posición de título no válida";
-        }
+    public function setApariencia($apariencia) {
+        $this->apariencia = $apariencia;
     }
 
-    // Método que se encarga de imprimir las propiedades del título
     public function imprimirPropiedades() {
         echo "Título: " . $this->titulo . "<br>";
         echo "Color: " . $this->color . "<br>";
         echo "Fuente: " . $this->fuente . "<br>";
     }
+
+    public function obtenerAparienciaTitulo() {
+        return $this->apariencia;
+    }
 }
 
-// Instanciar la clase CabeceraPagina e invocar sus métodos
-$cabecera = new CabeceraPagina();
-$cabecera->obtenerPropiedades("Mi Página", "Azul", "Arial");
-echo "Apariencia del título: " . $cabecera->obtenerAparienciaTitulo("centrado") . "<br>";
-$cabecera->imprimirPropiedades();
+// Ejemplo de uso
+$cabecera = new CabeceraPagina("Mi Página", "Azul", "Arial");
+$cabecera->setApariencia("centrado");
 
+echo "Apariencia del título: " . $cabecera->obtenerAparienciaTitulo() . "<br>";
+$cabecera->imprimirPropiedades();
 
 ?>
